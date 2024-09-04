cae055ee797930702ce8bd889b44f77b35897554
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2024-09-04 15:27:46 -0400

    Habilitar comando Giro a la Derecha(D)

--- src/auto.js
+++ src/auto.js
@@ -3,6 +3,18 @@ function ejecutarSecuencia(comandos){
     const [maxX, maxY] = dimensiones.split(',').map(Number);
     let [x, y, direccion] = [parseInt(posicionInicial[0]), parseInt(posicionInicial[2]), posicionInicial[3]];
 
+    for (let comando of secuencia) {
+        if (comando === 'D') {
+          direccion = girarDerecha(direccion);
+        }
+    }
+
     return `${x},${y}${direccion}`;
 }
+
+function girarDerecha(direccion) {
+    const giros = { 'N': 'E', 'E': 'S', 'S': 'O', 'O': 'N' };
+    return giros[direccion];
+  }
+
 export default ejecutarSecuencia;
--- src/auto.test.js
+++ src/auto.test.js
@@ -3,3 +3,7 @@ import ejecutarSecuencia from "./auto.js"
 test('Debe mostrar la posición inicial del auto', () => {
     expect(ejecutarSecuencia('5,5/1,2N/')).toBe('1,2N');
   });
+
+test('Debe girar a la derecha correctamente', () => {
+  expect(ejecutarSecuencia('5,5/1,2N/DDD')).toBe('1,2O');
+  });
