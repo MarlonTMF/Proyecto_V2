db579a269758d728f13d5b7a2a69137941563124
Reynaldo <reynaldo.ruiz@ucb.edu.bo>
2024-09-04 10:59:47 -0400

    Mostrar posicion inicial del auto que el usuario ingreso

--- src/auto.js
+++ src/auto.js
@@ -1,4 +1,8 @@
 function ejecutarSecuencia(comandos){
-    return "1,2N";
+    const [dimensiones, posicionInicial, secuencia] = comandos.split('/');
+    const [maxX, maxY] = dimensiones.split(',').map(Number);
+    let [x, y, direccion] = [parseInt(posicionInicial[0]), parseInt(posicionInicial[2]), posicionInicial[3]];
+
+    return `${x},${y}${direccion}`;
 }
 export default ejecutarSecuencia;
